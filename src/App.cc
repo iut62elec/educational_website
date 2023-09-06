d7f4beeeb1ce3400adee3b371114b4da76168a39
jpedram <jpedram@amazon.com>
2023-09-06 11:05:05 -0400

    .

---
+++ src/App.cc
@@ -0,0 +1,19 @@
+.App {
+  text-align: center;
+}
+
+.App-header {
+  background-color: #282c34;
+  padding: 20px;
+  color: white;
+}
+
+.video-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.video-container {
+  margin: 15px;
+}
--- src/App.js
+++ src/App.js
@@ -1,42 +1,95 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import './App.css';
 
-// Sample data: Replace these video IDs and categories with your own
 const videoData = {
-  'Category 1': ['videoID1', 'videoID2'],
-  'Category 2': ['videoID3', 'videoID4'],
-  'Category 3': ['videoID5', 'videoID6'],
-  'Category 4': ['videoID7', 'videoID8', 'videoID9'],
+  'معرفی': ['Giu1-k-Qkck'],
+  'نقشهای معلم حرفه ای': ['azNg43gqZ8c'],
+  'آموزش والدین برای تربیت کودک و نوجوان': ['Ehtt2IWYAk4'],
 };
+
+const HomePage = () => (
+  <div>
+    <p>Home Page Content</p>
+    {/* Embed the "معرفی" video */}
+    <iframe
+      title="معرفی"
+      width="560"
+      height="315"
+      src={`https://www.youtube.com/embed/Giu1-k-Qkck`}
+      frameBorder="0"
+      allowFullScreen
+    ></iframe>
+  </div>
+);
+
+const AboutPage = () => (
+  <div>
+    <p>About our academy and mission.</p>
+    {/* Embed the "معرفی" video */}
+    <iframe
+      title="معرفی"
+      width="560"
+      height="315"
+      src={`https://www.youtube.com/embed/Giu1-k-Qkck`}
+      frameBorder="0"
+      allowFullScreen
+    ></iframe>
+  </div>
+);
+
+const ContactPage = () => <div>Contact information will be here.</div>;
+
+const EducationalVideosPage = () => (
+  <div>
+    {['معرفی', 'نقشهای معلم حرفه ای'].map((category) => (
+      <div key={category} className="video-section">
+        <h2>{category}</h2>
+        <div className="video-list">
+          {videoData[category].map((videoId) => (
+            <div key={videoId} className="video-container">
+              <iframe
+                title={videoId}
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/${videoId}`}
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+            </div>
+          ))}
+        </div>
+      </div>
+    ))}
+  </div>
+);
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>My YouTube Video Collection</h1>
-      </header>
-      <main>
-        {Object.keys(videoData).map((category) => (
-          <div key={category}>
-            <h2>{category}</h2>
-            <div className="video-list">
-              {videoData[category].map((videoId) => (
-                <div key={videoId} className="video-container">
-                  <iframe
-                    title={videoId}
-                    width="560"
-                    height="315"
-                    src={`https://www.youtube.com/embed/${videoId}`}
-                    frameBorder="0"
-                    allowFullScreen
-                  ></iframe>
-                </div>
-              ))}
-            </div>
-          </div>
-        ))}
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>آکادمی آموزش معلمان و والدین</h1>
+          <h2>دکتر پوران خروشی</h2>
+        </header>
+
+        <nav className="App-nav">
+          <Link to="/">خانه</Link>
+          <Link to="/about">درباره</Link>
+          <Link to="/educational-videos">آموزشی ویدئوها</Link>
+          <Link to="/contact">تماس</Link>
+        </nav>
+
+        <main className="App-main">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/educational-videos" element={<EducationalVideosPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 };
 
